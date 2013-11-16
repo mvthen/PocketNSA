67bb0fdf036c9e8b4ceccb91760ab775e2d0d197
Mei-Vern Then <Mei-Vern@mei-verns-air.nyhq.nytint.com>
2013-11-16 16:03:02 -0500

    Added SMS func.

--- helloworld.py
+++ helloworld.py
@@ -22,6 +22,18 @@ class AddCompanies(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('added')
 
+class SendSMS(webapp2.RequestHandler):
+    def get(self):
+        # replace with your credentials from: https://www.twilio.com/user/account
+        account_sid = "ACe77d9c1b662b932535ee1a28277cda39"
+        auth_token = "0bbe3fb6b91364e4a6dd22eb2894ae96"
+        client = TwilioRestClient(account_sid, auth_token)
+        # replace "to" and "from_" with real numbers
+        rv = client.sms.messages.create(to="+19177038746",
+                                        from_="+19177468448",
+                                        body="Hello Monkey!")
+        self.response.write(str(rv))
+
 app = webapp2.WSGIApplication([
-                                  ('/twiml', HelloMonkey), ('/', MainPage), ('/addcompany', AddCompanies)],
+                                  ('/twiml', HelloMonkey), ('/send_sms', SendSMS), ('/', MainPage), ('/addcompany', AddCompanies)],
                               debug=True)
