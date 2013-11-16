5ed90073d4033dd324bbbd5c174c91cba89138c9
Mei-Vern Then <Mei-Vern@mei-verns-air.nyhq.nytint.com>
2013-11-16 15:08:57 -0500

    Test change of message to supp.

--- helloworld.py
+++ helloworld.py
@@ -5,7 +5,7 @@ from twilio import twiml
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('q-back says hello!')
+        self.response.write('q-back says suppp.')
 
 class HelloMonkey(webapp2.RequestHandler):
     def post(self):
---
+++ venv/bin/unit2.py
@@ -0,0 +1,7 @@
+#!/Users/Mei-Vern/Documents/Developer/Twilio/venv/bin/python
+
+__unittest = True
+
+from unittest2.main import main_
+
+main_()
