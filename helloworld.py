56f6a16fd4dc593cf1568dca10598dc061524d0c
Mei-Vern Then <Mei-Vern@mei-verns-air.nyhq.nytint.com>
2013-11-16 19:11:35 -0500

    conf calls tastic

--- helloworld.py
+++ helloworld.py
@@ -23,19 +23,35 @@ class HelloMonkey(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/xml'
         self.response.write(str(r))
 
+class RecordingTwimlet(webapp2.RequestHandler):
+    def get(self):
+        conf_name = self.request.get('conf_name')
+        recording_xml = '<Response><Dial record="true"><Conference>'+conf_name+'</Conference> </Dial></Response>'
+        self.response.headers['Content-Type'] = 'text/xml'
+        self.response.write(recording_xml)
+
 class TextResponse(webapp2.RequestHandler):
+
     def post(self):
         r = twiml.Response()
         account_sid = "ACe77d9c1b662b932535ee1a28277cda39"
         auth_token = "0bbe3fb6b91364e4a6dd22eb2894ae96"
         client = TwilioRestClient(account_sid, auth_token)
-        for message in client.messages.list():
-            print message.body
-        #fromNum = self.request.get('From')
-        ##message = client.messages.get("")
-        ##print message.body
-        #
-        #r.say(fromNum)
+        bodyMessage = self.request.get('Body')
+
+        returnCall = client.calls.create(to=self.request.get('From'),
+                                from_="+19177468448",
+                                url="http://twimlets.com/conference?Name="+self.request.get('From'))
+        call = client.calls.create(to=self.request.get('Body'),
+                                    from_="+19177468448",
+                                    url="http://twimlets.com/conference?Name="+self.request.get('From'))
+
+        recordingURL = "http://twimlets.com/echo?Twiml=%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%3F%3E%0A%3CResponse%3E%0A%20%20%3CDial%20record%3D%22true%22%3E%0A%20%20%20%20%3CConference%3E"+self.request.get('From')+"%3C%2FConference%3E%0A%20%20%3C%2FDial%3E%0A%3C%2FResponse%3E&"
+        recordCall = client.calls.create(to="+19177461446",
+                                       from_="+19177468448",
+                                     url=recordingURL)
+
+
         self.response.headers['Content-Type'] = 'text/xml'
         self.response.write(str(r))
 
@@ -53,14 +69,14 @@ class SendSMS(webapp2.RequestHandler):
         auth_token = "0bbe3fb6b91364e4a6dd22eb2894ae96"
         client = TwilioRestClient(account_sid, auth_token)
         # replace "to" and "from_" with real numbers
-        rv = client.sms.messages.create(to="+19177038746",
+        rv = client.calls.create(to="+19177038746",
                                         from_="+19177468448",
-                                        body="Hello Monkey!")
+                                        url="http://twimlets.com/conference")
         self.response.write(str(rv))
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello Monkey!')
 
 
 app = webapp2.WSGIApplication([
-                                  ('/twiml', HelloMonkey), ('/sms_response', TextResponse), ('/send_sms', SendSMS), ('/', MainPage), ('/addcompany', AddCompanies)],
+                                  ('/twiml', HelloMonkey),('/recording', RecordingTwimlet), ('/sms_response', TextResponse), ('/send_sms', SendSMS), ('/', MainPage), ('/addcompany', AddCompanies)],
                               debug=True)
